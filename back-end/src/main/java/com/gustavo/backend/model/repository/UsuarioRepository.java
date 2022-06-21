15bff46a493413cda4d21b7d935158a93bae902e
Gustavo Macedo <gustavo.macedo9@outlook.com>
2022-06-21 20:08:23 -0300

    implementando método: autenticarUsuario()

--- back-end/src/main/java/com/gustavo/backend/model/repository/UsuarioRepository.java
+++ back-end/src/main/java/com/gustavo/backend/model/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.gustavo.backend.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gustavo.backend.model.entity.Usuario;
@@ -8,4 +10,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	boolean existsByEmail(String email);
 	
+	Optional<Usuario> findByEmail(String email);
 }
--- back-end/src/main/java/com/gustavo/backend/service/impl/UsuarioServiceImpl.java
+++ back-end/src/main/java/com/gustavo/backend/service/impl/UsuarioServiceImpl.java
@@ -1,9 +1,12 @@
 package com.gustavo.backend.service.impl;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.gustavo.backend.exception.ErroAutenticacao;
 import com.gustavo.backend.exception.RegraNegocioException;
 import com.gustavo.backend.model.entity.Usuario;
 import com.gustavo.backend.model.repository.UsuarioRepository;
@@ -20,8 +23,17 @@ public class UsuarioServiceImpl implements UsuarioService {
 
 	@Override
 	public Usuario autenticar(String email, String senha) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Usuario> usuario = repository.findByEmail(email);
+		
+		if(!usuario.isPresent()) {
+			throw new ErroAutenticacao("Usuário não encontrado para o e-mail informado");
+		}
+
+		if(!usuario.get().getSenha().equals(senha)) {
+			throw new ErroAutenticacao("Senha inválida");
+		}
+		
+		return usuario.get();
 	}
 
 	@Override
