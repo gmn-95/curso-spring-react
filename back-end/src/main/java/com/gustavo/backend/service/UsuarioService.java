4a61b743bbd5f3fc2a1a1d928aba7d6f9916683d
Gustavo Macedo <gustavo.macedo9@outlook.com>
2022-06-21 18:11:58 -0300

    criando usuario service

---
+++ back-end/src/main/java/com/gustavo/backend/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.gustavo.backend.service;
+
+import com.gustavo.backend.model.entity.Usuario;
+
+public interface UsuarioService {
+
+	Usuario autenticar(String email, String senha);
+	
+	Usuario salvarUsuario(Usuario usuario);
+	
+	void validarEmail(String email);
+	
+}
