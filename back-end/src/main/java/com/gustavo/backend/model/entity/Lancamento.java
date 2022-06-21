77400fd16fff085bbb89ca5090585ed788ed600d
Gustavo Macedo <gustavo.macedo9@outlook.com>
2022-06-20 21:06:17 -0300

    adicionando o lombok

--- back-end/src/main/java/com/gustavo/backend/model/entity/Lancamento.java
+++ back-end/src/main/java/com/gustavo/backend/model/entity/Lancamento.java
@@ -2,7 +2,6 @@ package com.gustavo.backend.model.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
@@ -21,8 +20,13 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import com.gustavo.backend.model.entity.enums.StatusLancamento;
 import com.gustavo.backend.model.entity.enums.TipoLancamento;
 
+import lombok.Builder;
+import lombok.Data;
+
 @Entity
 @Table(name = "lancamento", schema = "financas")
+@Data
+@Builder
 public class Lancamento {
 
 	@Id
@@ -57,132 +61,5 @@ public class Lancamento {
 	@Column(name = "status")
 	@Enumerated(value = EnumType.STRING)
 	private StatusLancamento statusLancamento;
-
-	public Lancamento() {
-	}
-
-	public Lancamento(Long id, String descricao, Integer mes, Integer ano, Usuario usuario, BigDecimal valor,
-			LocalDate data_cadastro, TipoLancamento tipo, StatusLancamento statusLancamento) {
-		this.id = id;
-		this.descricao = descricao;
-		this.mes = mes;
-		this.ano = ano;
-		this.usuario = usuario;
-		this.valor = valor;
-		this.data_cadastro = data_cadastro;
-		this.tipo = tipo;
-		this.statusLancamento = statusLancamento;
-	}
-
-	public Lancamento(String descricao, Integer mes, Integer ano, Usuario usuario, BigDecimal valor,
-			LocalDate data_cadastro, TipoLancamento tipo, StatusLancamento statusLancamento) {
-		this.descricao = descricao;
-		this.mes = mes;
-		this.ano = ano;
-		this.usuario = usuario;
-		this.valor = valor;
-		this.data_cadastro = data_cadastro;
-		this.tipo = tipo;
-		this.statusLancamento = statusLancamento;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Integer getMes() {
-		return mes;
-	}
-
-	public void setMes(Integer mes) {
-		this.mes = mes;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public LocalDate getData_cadastro() {
-		return data_cadastro;
-	}
-
-	public void setData_cadastro(LocalDate data_cadastro) {
-		this.data_cadastro = data_cadastro;
-	}
-
-	public TipoLancamento getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoLancamento tipo) {
-		this.tipo = tipo;
-	}
-
-	public StatusLancamento getStatusLancamento() {
-		return statusLancamento;
-	}
-
-	public void setStatusLancamento(StatusLancamento statusLancamento) {
-		this.statusLancamento = statusLancamento;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(ano, data_cadastro, descricao, id, mes, statusLancamento, tipo, usuario, valor);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lancamento other = (Lancamento) obj;
-		return Objects.equals(ano, other.ano) && Objects.equals(data_cadastro, other.data_cadastro)
-				&& Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
-				&& Objects.equals(mes, other.mes) && statusLancamento == other.statusLancamento && tipo == other.tipo
-				&& Objects.equals(usuario, other.usuario) && Objects.equals(valor, other.valor);
-	}
-
-	@Override
-	public String toString() {
-		return "Lancamento [id=" + id + ", descricao=" + descricao + ", mes=" + mes + ", ano=" + ano + ", usuario="
-				+ usuario + ", valor=" + valor + ", data_cadastro=" + data_cadastro + ", tipo=" + tipo
-				+ ", statusLancamento=" + statusLancamento + "]";
-	}
 
 }
--- back-end/src/main/java/com/gustavo/backend/model/entity/Usuario.java
+++ back-end/src/main/java/com/gustavo/backend/model/entity/Usuario.java
@@ -1,7 +1,5 @@
 package com.gustavo.backend.model.entity;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +7,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Builder;
+import lombok.Data;
+
 @Entity
 @Table(name = "usuario", schema = "financas")
+@Data //lombok
+@Builder
 public class Usuario {
 
 	@Id
@@ -26,78 +29,6 @@ public class Usuario {
 
 	@Column(name = "senha")
 	private String senha;
-	
-	public Usuario() {
-	}
-	
-	public Usuario(Long id, String nome, String email, String senha) {
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
-	}
-
-	public Usuario(String nome, String email, String senha) {
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(id, other.id);
-	}
-
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
-	}
-	
-	
 	
 
 }
