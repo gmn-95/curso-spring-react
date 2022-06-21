3e73ae5a5b55608bf8e6028bbf9e2f87b017c8b2
Gustavo Macedo <gustavo.macedo9@outlook.com>
2022-06-21 20:23:50 -0300

    refatorando teste de usuario repository

--- back-end/src/test/java/com/gustavo/backend/model/repository/UsuarioRepositoryTest.java
+++ back-end/src/test/java/com/gustavo/backend/model/repository/UsuarioRepositoryTest.java
@@ -4,7 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,19 +19,28 @@ import com.gustavo.backend.model.entity.Usuario;
  * 
  * */
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @Profile("test")
+@DataJpaTest/*cria uma instancia do db na memoria e depois encerra, apenas no período do test.
+			* Dá rollback no final do test, ou seja, não salva as alterações
+			* */
+@AutoConfigureTestDatabase(replace = Replace.NONE)//usado para não desconfigurar o profile 'test'
 public class UsuarioRepositoryTest {
 	
 	@Autowired
 	private UsuarioRepository repository;
+	
+	/*
+	 * EntityManager configurado apenas para testes
+	 * */
+	@Autowired
+	private TestEntityManager entityManager;
 
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
 		//cenário
 		Usuario usuario = Usuario.builder().nome("usu").email("usu@gmail").build();
-		repository.save(usuario);
+		entityManager.persist(usuario);
 		
 		//ação-execução
 		boolean result = repository.existsByEmail(usuario.getEmail());
@@ -39,8 +51,6 @@ public class UsuarioRepositoryTest {
 	
 	@Test
 	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
-		//cenário
-		repository.deleteAll();
 		
 		//ação
 		boolean result = repository.existsByEmail("usu@gmail");
