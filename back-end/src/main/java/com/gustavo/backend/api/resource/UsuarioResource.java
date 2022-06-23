cfef83041b1d26be563035d8f8a910900e1f3270
Gustavo Macedo <gustavo.macedo9@outlook.com>
2022-06-23 19:12:43 -0300

    criando o controller rest de usuários

---
+++ back-end/src/main/java/com/gustavo/backend/api/resource/UsuarioResource.java
@@ -0,0 +1,13 @@
+package com.gustavo.backend.api.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioResource {
+	
+	@GetMapping("/")
+	public String helloWorld() {
+		return "hello world";
+	}
+}
