2af863cdc73d9d310919880b6fce792c267472d9
Gustavo Macedo <gustavo.macedo9@outlook.com>
2022-06-23 19:29:46 -0300

    método post de salvar usuario

---
+++ back-end/src/main/java/com/gustavo/backend/api/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.gustavo.backend.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter @Setter
+public class UsuarioDTO {
+
+	private String email;
+	private String nome;
+	private String senha;
+	
+}
--- back-end/src/main/java/com/gustavo/backend/api/resource/UsuarioResource.java
+++ back-end/src/main/java/com/gustavo/backend/api/resource/UsuarioResource.java
@@ -1,13 +1,40 @@
 package com.gustavo.backend.api.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gustavo.backend.api.dto.UsuarioDTO;
+import com.gustavo.backend.exception.RegraNegocioException;
+import com.gustavo.backend.model.entity.Usuario;
+import com.gustavo.backend.service.UsuarioService;
+
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioResource {
 	
-	@GetMapping("/")
-	public String helloWorld() {
-		return "hello world";
+	private UsuarioService service;
+	
+	public UsuarioResource(UsuarioService service) {
+		this.service = service;
+	}
+	
+	@PostMapping
+	public ResponseEntity salvar( @RequestBody UsuarioDTO dto) {
+		
+		Usuario usuario = Usuario.builder()
+						  .nome(dto.getNome())
+						  .email(dto.getEmail())
+						  .senha(dto.getSenha()).build();
+		
+		try {
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 }
