acbfdecbfd811b808a0c11496c36110e36a0282f
Gustavo Macedo <gustavo.macedo9@outlook.com>
2022-06-27 21:50:54 -0300

    implementando métodos de buscar lançamentos

--- back-end/src/main/java/com/gustavo/backend/api/resources/LancamentoResource.java
+++ back-end/src/main/java/com/gustavo/backend/api/resources/LancamentoResource.java
@@ -1,14 +1,19 @@
 package com.gustavo.backend.api.resources;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gustavo.backend.api.dto.LancamentoDTO;
@@ -63,6 +68,31 @@ public class LancamentoResource {
 			service.deletar(entity);
 			return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
 		}).orElseGet(() -> new ResponseEntity<>("Lançamento não encontrado na base de dados!", HttpStatus.BAD_REQUEST));
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> buscar(
+			@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam("usuario") Long idUsuario
+			){
+		
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+		
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+		if(!usuario.isPresent()) {
+			return ResponseEntity.badRequest().body("Não foi possível realziar a consulta! Usuário não encontrado para o Id informado!");
+		}
+		else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
 	}
 	
 	private Lancamento converter(LancamentoDTO dto) {
